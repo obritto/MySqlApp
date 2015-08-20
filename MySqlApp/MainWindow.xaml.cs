6202cddff78e6911d1ad34097daeac71985f15cf
Tiago de Mello Britto <obritto@hotmail.com>
2015-08-19 22:26:37 -0300

    MySqlApp

---
+++ MySqlApp/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MySqlApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Models.DadosModel db = new Models.DadosModel();
+
+            db.pessoa.Add(new Models.pessoa
+            {
+                nome = "TIAGO",
+                telefone = "123456"
+            });
+
+            db.SaveChanges();
+
+            var l = db.pessoa.ToList();
+
+            MessageBox.Show(l[0].nome);
+        }
+    }
+}
